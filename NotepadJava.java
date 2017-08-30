a4b6999ed90cae82c02555377afa466273ef603c
Brady <btannerw@gmail.com>
2017-08-30 10:27:24 -0400

    notepad program commit

---
+++ NotepadJava.java
@@ -0,0 +1,9 @@
+public class NotepadJava {
+
+	public static void main(String [] args) {
+		System.out.println("I love JAVA. JAVA is fun");
+		System.out.println("Or this way....");
+		System.out.println("I love JAVA.");
+		System.out.println("JAVA is fun.");
+	}
+}
